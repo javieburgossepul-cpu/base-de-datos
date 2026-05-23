8871358516756d5ba26acac8b8f7b18afb115c3b
javieburgossepul-cpu <229031027+javieburgossepul-cpu@users.noreply.github.com>
2026-05-22 21:12:58 -0500

    subido correctamente

--- EJERCICIOS/ejercicio_02_indices.sql
+++ EJERCICIOS/ejercicio_02_indices.sql
@@ -1,3 +1,9 @@
-EXPLAIN SELECT * FROM academia.profesores WHERE correo = 'profesor1@correo.com';
-CREATE INDEX idx_profesores_correo ON academia.profesores(correo); 
-EXPLAIN SELECT * FROM academia.profesores WHERE correo = 'profesor1@correo.com'; 
+EXPLAIN SELECT * FROM academia.profesores 
+WHERE correo = 'profesor1@correo.com';
+
+
+CREATE INDEX idx_profesores_correo 
+ON academia.profesores(correo); 
+
+EXPLAIN SELECT * FROM academia.profesores 
+WHERE correo = 'profesor1@correo.com'; 
